117005c89752e3722234dc08d1d3bc38d7835472
Hasan-new <mdhasan.yahoo@gmail.com>
2020-08-07 01:15:17 +0600

    comitted

---
+++ letconst.js
@@ -0,0 +1,31 @@
+const hubby = "Elias Kanchon";
+console.log(hubby);
+
+const numbers = [12, 45]
+numbers[1] = 88;
+numbers.push(12);
+const nayok = {name:"sakib khan", phone:4578};
+
+// console.log(numbers);
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+// console.log(patientName);
+
+
+// i ter result 10 hobe.
+// let sum =  0;
+//     for (var i = 0; i < 10; i ++ ){
+//         sum = sum + i;
+// }
+// console.log(i);
+
+
+// 18 no line let dea
+let sum =  0;
+    for (let i = 0; i < 10; i++){
+        sum = sum + i;
+}
+console.log(i);
+// result error debay. let k baire theke call kora jabay na.
+// var k out side theke call kora ji.
