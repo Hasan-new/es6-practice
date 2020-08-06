864448bd3c47655a733a3ae5b9392b763a20f932
Hasan-new <mdhasan.yahoo@gmail.com>
2020-08-07 01:47:48 +0600

    comitted

---
+++ template.js
@@ -0,0 +1,20 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName  = firstName + " " + lastName + " is a good boy";
+// empty sign ta delay dui name er majhkhane khali spase
+// toiri hobe
+const fullName2 = `${firstName} ${lastName} is a good boy.`;
+
+const multiLine = "I love you"
+    + "I miss you"
+    + "I need you"
+
+    const multiLine2 = `i love you
+    i miss you
+    no. i don't need you 
+    Baily road a dorkar ni.`
+
+    console.log(multiLine2);
+
+// console.log(fullName2);
+
